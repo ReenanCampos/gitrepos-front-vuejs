aaf30ff308aa322c0e129e9f31c8d744649a456a
Rere PC <reenan.campos@gmail.com>
2019-06-05 00:11:55 -0300

    RELEASE: Release v1.0 of GitRepo
    Clean project and not used methods

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <template>
+
   <div id="app">
     <h1>Git Repositories Info </h1>
-    
     <div>
       <div class="person search">
-        <!-- object value -->
-          <model-select :options="options"
-                                  v-model="item"
-                                  placeholder="select item">
-          </model-select>
+          <model-select :options="options" v-model="item" placeholder="Select a Language"> </model-select>
       </div>
     </div>
-
     <transition-group name="fade" tag="div" class="row">
       <div v-for="(repo, $key) in list" :key="$key" class="col-md-6 col-lg-4">
         <div class="person">
@@ -21,16 +16,14 @@
             <a target="_blank" class="person__name" v-bind:href="repo.urlrepositoryPath" >{{repo.fullRepositoryName}}</a>
           </div>
           <div class="person__email">
-            <p><b>Current Stars: </b>{{repo.currentStargazers}}</p>
-            <p><b>Current Forks: </b>{{repo.currentForks}}</p>
+            <p><b>Stars: </b>{{repo.currentStargazers | formatNumber}}</p>
+            <p><b>Forks: </b>{{repo.currentForks | formatNumber}}</p>
           </div>
           <br>
         </div>
       </div>
     </transition-group>
-
     <infinite-loading @infinite="infiniteHandler"></infinite-loading>
-
   </div>
  
 </template>
@@ -40,142 +33,75 @@ import axios from 'axios';
 import moment from 'moment';
 import InfiniteLoading from 'vue-infinite-loading';
 import { ModelSelect } from 'vue-search-select'
+
 import availableLanguages062019 from '../json/availableLanguages062019.json'
 
-const api = 'https://hn.algolia.com/api/v1/search_by_date?tags=story';
-const api2 = 'https://randomuser.me/api/';
-const api3 = 'https://api.github.com/search/repositories';
-const api4 = 'https://topi-renancampos.herokuapp.com/';
-const api5 = 'localhost:8081/api/GitRepository/findByFilter';
-
+const api = "https://topi-renancampos.herokuapp.com/api/GitRepository/findByFilter?";
 
 export default {
+
   name: 'app',
+
   data () {
     return {
-      persons: [],
-      msg: 'Teste TOPi',
-
       page: 1,
       list: [],
       language: 'Java',
-
-      people: [],
-      quantity: 6,
-      title:"RandomUser.me / Vue.js / Axios",
 
       options: availableLanguages062019,
       item: {
         value: 'Java',
         text: 'Java'
-      },
-
-      dadosTeste:
-      [
-        {
-          "avatar_url": "https://avatars3.githubusercontent.com/u/36260787?v=4",
-          "full_name": "CyC2018/CS-Notes",
-          "stargazers_count": 64689,
-          "forks_count": 19816,
-          "html_url":"https://github.com/CyC2018/CS-Notes"
-        },
-        {
-          "avatar_url": "https://avatars1.githubusercontent.com/u/582346?v=4",
-          "full_name": "iluwatar/java-design-patterns",
-          "stargazers_count": 48081,
-          "forks_count": 15579,
-          "html_url":"https://github.com/iluwatar/java-design-patterns"
-        },
-        {
-          "avatar_url" : "https://avatars0.githubusercontent.com/u/29880145?v=4",
-          "full_name" : "Snailclimb/JavaGuide",
-          "stargazers_count": 42293,
-          "forks_count": 13365,
-          "html_url":"https://github.com/Snailclimb/JavaGuide"
-        }
-      ],
-
+      }
     }
   },
-  methods: {
-      reset () {
-        this.item = {}
-      },
-      selectFromParentComponent1 () {
-        // select option from parent component
-        this.item = this.options[0]
-        console.log("selecionado:" + this.item.value);
-      },
-    /*getInitialUsers () {
-      for (var i = 0; i < 5; i++) {
-        console.log(i);
-        axios.get(`https://randomuser.me/api/`)
-          .then(response => {
-            this.persons.push(response.data.results[0]);
-          });
-      }
-    },
-    scroll (person) {
-      window.onscroll = () => {
-        let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
 
-        if (bottomOfWindow) {
-          axios.get(`https://randomuser.me/api/`)
-            .then(response => {
-              this.persons.push(response.data.results[0]);
-            });
-        }
-      };
-    },*/
+  methods: {
+    reset () {
+      this.item = {}
+    },
+    selectFromParentComponent1 () {
+      this.item = this.options[0]
+    },
 
     infiniteHandler($state) {
-      console.log("bora");
-      axios.get("https://topi-renancampos.herokuapp.com/api/GitRepository/findByFilter?" + "language=" + this.language + "&page=" + this.page).then(({ data }) => {
-        console.log("Puxou! -> ", data);
-        console.log("Pagina! -> ", this.page);
+      axios.get(api + "language=" + this.language + "&page=" + this.page).then(({ data }) => {
         this.page += 1;
         this.list.push(...data.items);
         $state.loaded();
       })
-    },
-    
-    resetRepositories(){
-      this.page = 1;
-      this.list = [];
     }
 
   },
-  /*beforeMount() {
-    this.getInitialUsers();
-    console.log(this.persons);
-  },
-  mounted() {
-  this.scroll(this.person);
-  },*/
-  created: function () {
-    this.getPeople();
-  },
+
   components: {
     ModelSelect
   },
+
   watch: {
     item: function(item) {
-
+      
+      // Replacing blank spaces " " to char "-"
       item.value = item.value.replace("\\s+","-");
-
-      console.log("value: " + item.value);
       this.language = item.value;
       this.page = 1;
       this.list = [];
-      axios.get("https://topi-renancampos.herokuapp.com/api/GitRepository/findByFilter?" + "language=" + this.item.value + "&page=" + this.page).then(({ data }) => {
+      
+      // Refrash list to get again the API with new value
+      axios.get(api + "language=" + this.item.value + "&page=" + this.page).then(({ data }) => {
         this.page += 1;
         this.list.push(...data.items);
-        $state.loaded();
       })
 
     }
-  }
+  },
 
+  filters:{
+    formatNumber: function(value){
+      var a = Number(value);
+      return a.toLocaleString();
+    }
+  }
 }
 </script>
 
@@ -192,7 +118,6 @@ export default {
 
     a{
       color: rgb(95, 95, 168);
-    
     }
 
     word-break: break-all;
@@ -237,6 +162,5 @@ export default {
     }
 
   }
-
-
+  
 </style>
