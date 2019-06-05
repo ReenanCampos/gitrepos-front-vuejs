83cca06b2859f5bee7f7b3a542baa63c461fcc06
Rere PC <reenan.campos@gmail.com>
2019-06-04 23:14:41 -0300

    FEAT: Change of mock data to API JAVA data
    Using a real API backend

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import moment from 'moment'
 import InfiniteLoading from 'vue-infinite-loading';
+import Multiselect from 'vue-multiselect'
 
 Vue.component('InfiniteLoading', InfiniteLoading);
+Vue.component('multiselect', Multiselect)
 
 new Vue({
   el: '#app',
